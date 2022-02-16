81440c950b741510c3956fe5479d872f99923b08
maklad42 <maklad42+git@gmail.com>
2022-02-16 21:11:37 +0900

    Adds copy button scripts, and code to clear the results pane. 220216

---
+++ scripts/uuid.js
@@ -0,0 +1,64 @@
+const cpyBtn = document.getElementById('cpyBtn');
+cpyBtn.addEventListener('click', copyIds);
+
+const clrBtn = document.querySelector('.clearBtn');
+clrBtn.addEventListener('click', clearResults);
+
+function getIds() {
+  const url = './generate.php';
+
+  fetch(url, {
+    method: 'POST',
+    body: new FormData(document.getElementById('inputform')),
+  })
+    .then(
+      (response) => response.text() // .json(), etc.
+    )
+    .then((html) => {
+      document.getElementById('results').innerHTML = html;
+      document.getElementById('txt').value = html.replace(/<br \/>/g, '\n');
+      document.querySelector('.clearBtn').classList.remove('hidden');
+      document.getElementById('cpyBtn').classList.add('active');
+    });
+}
+
+function copyIds(e) {
+  console.log();
+  let btn = document.getElementById('cpyBtn');
+  if (btn.classList.contains('active')) {
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+      const uuids = document.getElementById('txt').value;
+      navigator.clipboard.writeText(uuids);
+    } else {
+      window
+        .getSelection()
+        .selectAllChildren(document.getElementById('results'));
+      document.execCommand('copy');
+      clearSelection();
+    }
+    document.querySelector('.copyres').innerHTML = 'Copied!';
+  }
+}
+
+function clearSelection() {
+  if (window.getSelection) {
+    if (window.getSelection().empty) {
+      // Chrome
+      window.getSelection().empty();
+    } else if (window.getSelection().removeAllRanges) {
+      // Firefox
+      window.getSelection().removeAllRanges();
+    }
+  } else if (document.selection) {
+    // IE?
+    document.selection.empty();
+  }
+}
+
+function clearResults() {
+  document.getElementById('results').innerHTML = '';
+  document.getElementById('txt').value = '';
+  document.querySelector('.clearBtn').classList.add('hidden');
+  document.getElementById('cpyBtn').classList.remove('active');
+  document.querySelector('.copyres').innerHTML = '';
+}
